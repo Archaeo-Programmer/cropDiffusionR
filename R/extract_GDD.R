d76bf28d525e43cf40185b76257593f797ee5d93
Andrew Gillreath-Brown <andrew.brown1234@gmail.com>
2022-09-22 21:01:12 -0700

    updated extract gdd function to work off the already loaded gdd data in the package.

--- R/extract_GDD.R
+++ R/extract_GDD.R
@@ -12,20 +12,8 @@ extract_GDD <-
   function(sites) {
 
     # Get annual accumulated growing degree days.
-    raster::extract(x = cropDiffusionR::NASW_gdd,
+    terra::extract(x = cropDiffusionR::NASW_gdd,
                     y = sites) %>%
-      `colnames<-`(month.name) %>%
-      dplyr::bind_cols(LabID = sites$LabID, .) %>%
-      tidyr::pivot_longer(-LabID) %>%
-      dplyr::left_join(., month_days, by = c("name" = "month")) %>%
-      dplyr::mutate(value = dplyr::case_when(value < 10.00 ~ 10.00,
-                                             value > 30.00 ~ 30.00,
-                                             TRUE ~ value)) %>%
-      dplyr::rowwise() %>%
-      dplyr::mutate(month_GDD = (value - 10.00) * days) %>%
-      dplyr::group_by(LabID) %>%
-      dplyr::summarise(gdd = sum(month_GDD)) %>%
-      dplyr::left_join(., sites, by = "LabID") %>%
-      dplyr::filter(!is.na(gdd))
+      dplyr::bind_cols(LabID = sites$LabID, GDD = .)
     
   }
